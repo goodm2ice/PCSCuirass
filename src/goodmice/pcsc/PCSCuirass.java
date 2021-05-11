c58a302032941b991e75b8139e789e0994310c68
goodm2ice <hamel2517@gmail.com>
2021-05-11 23:16:44 +0500

    Initial commit

---
+++ src/goodmice/pcsc/PCSCuirass.java
@@ -0,0 +1,17 @@
+package goodmice.pcsc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PCSCuirass extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		//
+	}
+	
+	@Override
+	public void onDisable() {
+		//
+	}
+	
+}
---
+++ src/goodmice/pcsc/commands/CommandShop.java
@@ -0,0 +1,28 @@
+package goodmice.pcsc.commands;
+
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import goodmice.pcsc.PCSCuirass;
+
+final public class CommandShop extends BaseCommand {
+
+	public CommandShop(PCSCuirass plugin) {
+		super(plugin);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
